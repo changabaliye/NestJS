35bc8c70ede1852018206af34072f158373b07f4
Abhishek Satapathy <chandransu.work.01@gmail.com>
2025-07-08 22:23:40 +0000

    CRUD COMPLETED

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,23 +1,43 @@
-import { Controller,Get, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, Query } from '@nestjs/common';
 import { PostService } from './post.service';
-import { Post as PostInterface} from './interfaces/post.interface';
-import { single } from 'rxjs';
+import { Post as PostInterface } from './interfaces/post.interface';
 
 @Controller('post')
 export class PostController {
-    constructor(private readonly postService: PostService) {}
+    constructor(private readonly postService: PostService) { }
 
     @Get()
-    findAll(@Query('search') search? :string): PostInterface[] {
+    findAll(@Query('search') search?: string): PostInterface[] {
         const extractAllPosts = this.postService.findAll();
 
-        if(search) {
+        if (search) {
             return extractAllPosts.filter(singlePost => singlePost.title.toLowerCase().includes(search))
         }
 
         return extractAllPosts;
     }
 
-    @
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number): PostInterface {
+        return this.postService.findOne(id);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() createPostData: Omit<PostInterface, 'id' | 'createdAt'>): PostInterface {
+        return this.postService.create(createPostData);
+    }
+
+    @Put(':id')
+    update(@Param('id', ParseIntPipe) id: number,
+        @Body() updatedData: Partial<Omit<PostInterface, 'id' | 'createdAt'>>): PostInterface {
+            return this.postService.update(id,updatedData)
+    }
+
+    @Delete(":id")
+    @HttpCode(HttpStatus.NO_CONTENT)
+    remove(@Param('id',ParseIntPipe) id : number) : {message : string} {
+        return this.postService.remove(id);
+    }
 }
- 
+
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -3,26 +3,74 @@ import { Post } from './interfaces/post.interface';
 
 @Injectable()
 export class PostService {
-    private posts : Post[] = [
+    private posts: Post[] = [
         {
-            id : 1,
-            title : 'First Post',
-            content : 'This is the content of the first post.',
-            authorName : 'Author One',
-            createdAt : new Date(),
+            id: 1,
+            title: 'First Post',
+            content: 'This is the content of the first post.',
+            authorName: 'Author One',
+            createdAt: new Date(),
         },
     ];
 
-    findAll() : Post[] {
+    findAll(): Post[] {
         return this.posts;
     }
 
-    findOne(id : number):Post {
+    findOne(id: number): Post {
         const singlePost = this.posts.find(post => post.id === id)
 
-        if(!singlePost) {
+        if (!singlePost) {
             throw new NotFoundException(`Post With ID ${id} is not found`);
         }
         return singlePost;
+    }
+
+    create(createPostData: Omit<Post, 'id' | 'createdAt'>): Post {
+        const newPost: Post = {
+            id: this.getNextId(),
+            ...createPostData,
+            createdAt: new Date()
+        }
+        this.posts.push(newPost);
+        return newPost;
+    }
+
+    update(id : number, updatePostData : Partial<Omit<Post, 'id' | 'createdAt'>>) : Post {
+        const postToEdit = this.posts.findIndex(post => post.id === id);
+
+        if(postToEdit) {
+            throw new NotFoundException(`Post With ID ${id} is not found`);
+        }
+
+        this.posts[postToEdit] = {
+            ...this.posts[postToEdit],
+            ...updatePostData,
+            updatedAt : new Date()
+        }
+
+        return this.posts[postToEdit];
+    }
+
+    remove(id:number) : {message : string} {
+        const postIndex = this.posts.findIndex(post => post.id === id);
+
+        if(postIndex) {
+            throw new NotFoundException(`Post With ID ${postIndex} is not found`);
+        }
+
+        this.posts = this.posts.filter(post => post.id !== id);
+
+        const response = {
+            message : 'removed succesfully'
+        }
+
+        return response;
+
+    }
+
+    private getNextId(): number {
+        return this.posts.length > 0 ?
+            Math.max(...this.posts.map(post => post.id)) + 1 : 1;
     }
 }
