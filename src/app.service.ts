97ff0c85c44fd594fb8e505a16f2d74e42a31f34
Abhishek Satapathy <chandransu.work.01@gmail.com>
2025-07-08 20:28:34 +0000

    environment managment using .env and @nest/config

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  // injection of configService provide by next js config
+  ig
+  constructor(private configService: ConfigService) { }
+
+
   getHello(): string {
-    return 'Hello World!';
+    // const appName = this.configService.get<string>('APP_NAME', 'default value')
+    const appName = this.configService.get<string>('appName')
+    console.log(appName, 'appName')
+    return `Hello ${appName}`;
   }
 }
